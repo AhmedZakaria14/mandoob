4d637184b84b84b7dea176e0eefd4e86a160ed8f
AhmedZakaria14 <azeco77@gmail.com>
2026-06-15 20:04:12 +0300

    chore: update README and add Google Tag Manager
    Update project documentation with deployment instructions and integrate Google Tag Manager for analytics tracking.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Cairo } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 
 const cairo = Cairo({
@@ -79,6 +80,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta name="theme-color" content="#6366f1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TF6WR9DF');
+          `}
+        </Script>
       </head>
       <body className="bg-brand-gray text-[#444] font-sans antialiased" suppressHydrationWarning>
         {children}
