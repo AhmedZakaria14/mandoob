f90682aede3778734099c31802a7837b5b1b575d
AhmedZakaria14 <azeco77@gmail.com>
2026-07-21 14:42:18 +0300

    Add 12 SEO-optimized blog articles

---
+++ data/articles/index.ts
@@ -0,0 +1,27 @@
+import article39 from './39.json';
+import article40 from './40.json';
+import article41 from './41.json';
+import article42 from './42.json';
+import article43 from './43.json';
+import article44 from './44.json';
+import article45 from './45.json';
+import article46 from './46.json';
+import article47 from './47.json';
+import article48 from './48.json';
+import article49 from './49.json';
+import article50 from './50.json';
+
+export const newBlogPosts = [
+  article39,
+  article40,
+  article41,
+  article42,
+  article43,
+  article44,
+  article45,
+  article46,
+  article47,
+  article48,
+  article49,
+  article50,
+];
